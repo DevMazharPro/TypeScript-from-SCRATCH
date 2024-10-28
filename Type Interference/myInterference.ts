b38693320f7729381a14b5f5c42c3040cd2527ad
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-10-28 18:59:33 +0500

    try

--- Type Interference/myInterference.ts
+++ Type Interference/myInterference.ts
@@ -6,10 +6,15 @@ interface User {
     readonly dbEmployeeId: number;
 };
 
+interface User {
+    githubID: string;
+}
+
 const user: User = {
     name: 'Faahad Lashari',
     age: 25,
     contact: 1234567890,
     address: 'City Hyderabad',
     dbEmployeeId: 12345,
+    githubID: 'faahadlashari'
 }
