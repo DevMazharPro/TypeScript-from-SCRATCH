98ed1e3e9acd950549b04faa249628d0e0888453
DevMazharPro <ibrarhalepotooo@gmail.com>
2024-10-29 18:36:56 +0500

    ignore

--- Classes, Get/getters.ts
+++ Classes, Get/getters.ts
@@ -20,3 +20,5 @@ const newUser =  new User('h@hgmail.com', 'Mazhar', 13);
 console.log(newUser.name); // Output: Mazhar
 console.log(newUser.email); // Output: h@hgmail.com
 console.log(newUser.age); // Output: User { email: 'h@hgmail.com', name: 'Mazhar', age: 13 }
+
+export{}
